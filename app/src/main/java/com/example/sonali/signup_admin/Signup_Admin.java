c821cc9569074efada1b4967b6128b51850dd19e
unknown <salonicool>
2018-07-22 15:41:53 +0530

    sONALI

---
+++ app/src/main/java/com/example/sonali/signup_admin/Signup_Admin.java
@@ -0,0 +1,13 @@
+package com.example.sonali.signup_admin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Signup_Admin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup__admin);
+    }
+}
